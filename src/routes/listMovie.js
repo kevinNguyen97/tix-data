de6021aa0f577f2b9949fd1f99e23ad639c4c484
kevin <xaythixin@gmail.com>
2020-06-04 13:52:44 +0700

    check maPhim is already in DB

--- src/routes/listMovie.js
+++ src/routes/listMovie.js
@@ -21,7 +21,7 @@ router.post('/ThemPhimDangChieu', async (req, res) => {
     const { error } = phimValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     // check maPhim is already in database
-    const maPhimExist = await CommingSoonMovie.findOne({ maPhim: req.body.details.maPhim });
+    const maPhimExist = await CommingSoonMovie.findOne({ maPhim: req.body.details.film_id });
 
     if (maPhimExist)
         return res.status(400).send('ma phim da ton tai');
@@ -50,7 +50,7 @@ router.post('/ThemPhimSapChieu', async (req, res) => {
     const { error } = phimValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     // check maPhim is already in database
-    const maPhimExist = await CommingSoonMovie.findOne({ maPhim: req.body.details.maPhim });
+    const maPhimExist = await CommingSoonMovie.findOne({ maPhim: req.body.details.film_id });
     if (maPhimExist)
         return res.status(400).send('ma phim da ton tai');
 
